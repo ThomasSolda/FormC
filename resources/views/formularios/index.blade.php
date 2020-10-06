7fb41e9fe48eb49c9d40b91d9927138a25a6bfbe
ThomasSolda <thomassolda@icloud.com>
2020-10-06 18:01:17 -0300

    Cambios css lista de noticias

--- resources/views/formularios/index.blade.php
+++ resources/views/formularios/index.blade.php
@@ -8,44 +8,47 @@
     </div>
 </div>
 
-<table class="border-separate border-2 text-center border-gray-500 mt-3 responsive" style="width: 100%">
-    <thead>
-    <tr>
-        <th class="px-4 py-2">{{ __("id") }}</th>
-        <th class="px-4 py-2">{{ __("fecha") }}</th>
-        <th class="px-4 py-2">{{ __("mes") }}</th>
-        <th class="px-4 py-2">{{ __("lugar") }}</th>
-        <th class="px-4 py-2">{{ __("localidad") }}</th>
-        <th class="px-4 py-2">{{ __("barrio") }}</th>
-        <th class="px-4 py-2">{{ __("tipodehecho") }}</th>
-        <th class="px-4 py-2">{{ __("personasidentificadas") }}</th>
-        <th class="px-4 py-2">{{ __("clanasociado") }}</th>
-        <th class="px-4 py-2">{{ __("movil") }}</th>
-        <th class="px-4 py-2">{{ __("observaciones") }}</th>
-        <th class="px-4 py-2">{{ __("linkdeorigen") }}</th>
-        <th class="px-4 py-2">{{ __("anio") }}</th>
-        <th class="px-4 py-2">{{ __("personasidentificadas") }}</th>
-    </tr>
+<table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+
+    <thead class="text-white">
+      <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+          <th class="p-3 text-left">{{ __("id") }}</th>
+          <th class="p-3 text-left">{{ __("fecha") }}</th>
+          <th class="p-3 text-left">{{ __("mes") }}</th>
+          <th class="p-3 text-left">{{ __("lugar") }}</th>
+          <th class="p-3 text-left">{{ __("localidad") }}</th>
+          <th class="p-3 text-left">{{ __("barrio") }}</th>
+          <th class="p-3 text-left">{{ __("tipodehecho") }}</th>
+          <th class="p-3 text-left">{{ __("personasidentificadas") }}</th>
+          <th class="p-3 text-left">{{ __("clanasociado") }}</th>
+          <th class="p-3 text-left">{{ __("movil") }}</th>
+          <th class="p-3 text-left">{{ __("observaciones") }}</th>
+          <th class="p-3 text-left">{{ __("linkdeorigen") }}</th>
+          <th class="p-3 text-left">{{ __("anio") }}</th>
+          <th class="p-3 text-left">{{ __("personasidentificadas") }}</th>
+          <th class="p-3 text-left">Acciones</th>
+      </tr>
     </thead>
-    <tbody>
+
+    <tbody class="flex-1 sm:flex-none">
         @forelse($forms as $form)
-            <tr>
-                <td class="border px-4 py-2">{{ $form->id }}</td>
-                <td class="border px-4 py-2">{{ $form->fecha }}</td>
-                <td class="border px-4 py-2">{{ $form->mes }}</td>
-                <td class="border px-4 py-2">{{ $form->lugar }}</td>
-                <td class="border px-4 py-2">{{ $form->localidad }}</td>
-                <td class="border px-4 py-2">{{ $form->barrio }}</td>
-                <td class="border px-4 py-2">{{ $form->tipodehecho }}</td>
-                <td class="border px-4 py-2">{{ $form->personasidentificadas }}</td>
-                <td class="border px-4 py-2">{{ $form->clanasociado }}</td>
-                <td class="border px-4 py-2">{{ $form->movil }}</td>
-                <td class="border px-4 py-2">{{ $form->observaciones }}</td>
-                <td class="border px-4 py-2">{{ $form->linkdeorigen }}</td>
-                <td class="border px-4 py-2">{{ $form->anio }}</td>
-                <td class="border px-4 py-2">{{ $form->personasidentificadas }}</td>
-                <td class="border px-4 py-2">
-                    <a href="{{ route("formularios.edit", ["form" => $form])}}" class="text-blue-400"> {{ __("Editar") }}</a>
+            <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->id }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->fecha }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->mes }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->lugar }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->localidad }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->barrio }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->tipodehecho }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->personasidentificadas }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->clanasociado }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->movil }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->observaciones }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->linkdeorigen }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->anio }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $form->personasidentificadas }}</td>
+                <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
+                    <a href="{{ route("formularios.edit", ["form" => $form])}}" class="text-green-400"> {{ __("Editar") }}</a>
                 </td>
             </tr>
         @empty
@@ -67,5 +70,33 @@
     </div>
 @endif
 
+<style>
+  html,
+  body {
+    height: 100%;
+  }
+
+  @media (min-width: 640px) {
+    table {
+      display: inline-table !important;
+    }
+
+    thead tr:not(:first-child) {
+      display: none;
+    }
+  }
+
+  td:not(:last-child) {
+    border-bottom: 0;
+  }
+
+  th:not(:last-child) {
+    border-bottom: 2px solid rgba(0, 0, 0, .1);
+  }
+  th {
+    background-color: #233876;
+    font-size:
+  }
+</style>
 
 @endsection
